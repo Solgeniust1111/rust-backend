03011dd38b0fd31ef7d0ff8454aa1dcb1f3a18a5
Solgeniust1111 <geniussoldev1111@gmail.com>
2025-03-26 07:53:25 +0100

    feat: add gitignore

--- target/debug/deps/rust_backend-455c7102e1338bb0.d
+++
@@ -1,5 +0,0 @@
-/home/sammy/work/rust-backend/target/debug/deps/librust_backend-455c7102e1338bb0.rmeta: src/main.rs
-
-/home/sammy/work/rust-backend/target/debug/deps/rust_backend-455c7102e1338bb0.d: src/main.rs
-
-src/main.rs:
--- target/debug/rust-backend.d
+++
@@ -1,1 +0,0 @@
-/home/sammy/work/rust-backend/target/debug/rust-backend: /home/sammy/work/rust-backend/src/main.rs
